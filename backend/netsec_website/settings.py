c2ddf7f637eb464eec38225383d8fe9706edcd99
Lexicalerror <b12902116@csie.ntu.edu.tw>
2025-04-14 13:09:54 +0800

    Moved backend files into /backend

---
+++ backend/netsec_website/settings.py
@@ -0,0 +1,170 @@
+
+from pathlib import Path
+# import environ
+import os
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# django-environ
+env = environ.Env(
+    DEBUG=(bool, False) 
+)
+environ.Env.read_env(env_file=BASE_DIR / '.env.local')
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY_FALLBACK = env('SECRET_KEY_FALLBACK')
+SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY_FALLBACKS = [
+    os.environ["SECRET_KEY_FALLBACK"],
+]
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+ALLOWED_HOSTS = [
+        # fakelocal.api"
+        '127.0.0.1', 
+        '.vercel.app'
+    ]
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'api',
+    # "corsheaders",
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'netsec_website.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# WSGI_APPLICATION = 'netsec_website.wsgi.application'
+WSGI_APPLICATION = 'netsec_website.wsgi.app'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+# STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS
+# CORS_ALLOWED_ORIGINS = [
+#     "http://fakelocal.api:5173",
+# ]
+
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://fakelocal.api:5173",
+# ]
+
+# CORS_ALLOW_HEADERS = [
+#     "content-type",
+#     "x-csrftoken",
+#     "authorization",
+#     "accept",
+#     "origin",
+#     "user-agent",
+#     "x-requested-with",
+# ]
+# CORS_ALLOW_CREDENTIALS = True
+
+# Vercel blob
+VERCEL_BLOB_BASE_URL = env('VERCEL_BLOB_BASE_URL')
+BLOB_READ_WRITE_TOKEN = env('BLOB_READ_WRITE_TOKEN')
+
+
+AUTH_USER_MODEL = 'api.CustomUser'
+
+# SSL
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
